bd6471635fc8ce6f7f20513fb8ec59fd37b906d3
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-11 19:54:37 +0100

    Add sidebar menu open and close functions

--- dashboard.js
+++ dashboard.js
@@ -417,3 +417,14 @@ function renderEvents() {
 }
 
 renderEvents();
+// ==========================
+// Sidebar Menu
+// ==========================
+
+function openMenu() {
+    document.getElementById("sideMenu").style.left = "0";
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").style.left = "-280px";
+}
