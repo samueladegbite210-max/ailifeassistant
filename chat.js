581deaeb796f50505dcc8df94e31a70cf329de1d
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-17 07:37:34 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -819,6 +819,72 @@ else if(/^[0-9+\-*/(). x]+$/i.test(msg)){
     }
 
 }
+    // ================================
+// Date & Time
+// ================================
+
+else if(
+    msg.includes("what is today's date") ||
+    msg.includes("what is the date") ||
+    msg.includes("today's date")
+){
+
+    reply = "📅 Today is " + new Date().toDateString();
+
+}
+
+else if(
+    msg.includes("what time is it") ||
+    msg.includes("current time") ||
+    msg.includes("time now")
+){
+
+    reply = "🕒 The current time is " +
+    new Date().toLocaleTimeString();
+
+}
+
+else if(
+    msg.includes("what day is today")
+){
+
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+
+    reply = "📆 Today is " + days[new Date().getDay()];
+
+}
+
+else if(
+    msg.includes("what month is this")
+){
+
+    const months = [
+        "January","February","March","April",
+        "May","June","July","August",
+        "September","October","November","December"
+    ];
+
+    reply = "📅 This month is " +
+    months[new Date().getMonth()];
+
+}
+
+else if(
+    msg.includes("what year is it")
+){
+
+    reply = "📅 The current year is " +
+    new Date().getFullYear();
+
+}
 // ================================
 // Default Reply
 // ================================
