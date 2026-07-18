9ae1124ac01e656ce6d6bbd31b60d70126ee276a
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-18 15:00:19 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+function aiReply(text){
 
+    const msg = text.toLowerCase().trim();
+
+    let reply =
+        conversationReply(msg,text) ||
+        knowledgeReply(msg,text) ||
+        taskReply(msg,text) ||
+        goalReply(msg,text) ||
+        noteReply(msg,text) ||
+        eventReply(msg,text) ||
+        memoryReply(msg,text) ||
+        calculatorReply(msg,text) ||
+        dateReply(msg,text) ||
+        weatherReply(msg,text) ||
+        "🤖 I'm still learning.";
+
+    addMessage("ai", reply);
+
+}
