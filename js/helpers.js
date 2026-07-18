f63ff75275839859514e578a17a20b575899323d
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-18 14:47:35 +0100

    Update helpers.js

--- js/helpers.js
+++ js/helpers.js
@@ -1,1 +1,25 @@
+// ================================
+// Helper Functions
+// ================================
 
+function hasAny(msg, words){
+
+    return words.some(function(word){
+        return msg.includes(word);
+    });
+
+}
+
+function saveData(key, value){
+
+    localStorage.setItem(key, JSON.stringify(value));
+
+}
+
+function loadData(key, defaultValue){
+
+    const data = localStorage.getItem(key);
+
+    return data ? JSON.parse(data) : defaultValue;
+
+}
