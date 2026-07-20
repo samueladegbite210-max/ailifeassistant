e54996c04d1ead2f86cdc1cdd6aa2b2cf0e7fd1c
samueladegbite210-max <samueladegbite210@gmail.com>
2026-07-20 20:49:58 +0100

    Create intents.js

---
+++ js/js/js/intents.js
@@ -0,0 +1,50 @@
+alert("intents.js loaded");
+
+const intents = {
+
+    greeting: [
+        "hi",
+        "hello",
+        "hey",
+        "good morning",
+        "good afternoon",
+        "good evening",
+        "good night"
+    ],
+
+    food: [
+        "eat",
+        "food",
+        "hungry",
+        "meal",
+        "breakfast",
+        "lunch",
+        "dinner"
+    ],
+
+    sleep: [
+        "sleep",
+        "sleeping",
+        "tired",
+        "rest"
+    ],
+
+    creator: [
+        "who create you",
+        "who created you",
+        "who made you",
+        "who built you"
+    ],
+
+    identity: [
+        "who are you",
+        "what are you",
+        "tell me about yourself"
+    ],
+
+    knowme: [
+        "do you know me",
+        "who am i"
+    ]
+
+};
