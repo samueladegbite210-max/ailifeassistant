f5fba76288aac40798b2ab86b4d6c3cb06a824ea
samueladegbite210-max <samueladegbite210@gmail.com>
2026-08-07 07:16:24 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -701,3 +701,18 @@ function getUploadedFiles(){
     return uploadedFiles;
 
 }
+function escapeHTML(value){
+
+    return String(value)
+
+        .replace(/&/g,"&amp;")
+
+        .replace(/</g,"&lt;")
+
+        .replace(/>/g,"&gt;")
+
+        .replace(/"/g,"&quot;")
+
+        .replace(/'/g,"&#039;");
+
+}
